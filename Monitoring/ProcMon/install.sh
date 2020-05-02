2a022d21a13817d82153d5490969a51ea8eed1b8
Edward Klesel <edwardklesel@gmail.com>
2020-05-02 19:46:37 +0100

    Modify install.sh

--- Monitoring/ProcMon/install.sh
+++ Monitoring/ProcMon/install.sh
@@ -1,12 +1,12 @@
 echo "Installing ProcMon..."
 cd /tmp
-wget https://github.com/edklesel/Pi-Suite/Monitoring/ProcMon/procmon.sh
+wget https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/ProcMon/procmon.sh
 mv procmon.sh procmon
 chmod u+x procmon
 mkdir/opt/monitoring
 mkdir /opt/monitoring/procmon
 mv procmon /opt/monitoring/procmon
 
-wget https://github.com/edklesel/Pi-Suite/Monitoring/ProcMon/procmon.service
+wget https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/ProcMon/procmon.service
 mv procmon.service /etc/systemd/system/procmon.service
 systemctl daemon-reload
