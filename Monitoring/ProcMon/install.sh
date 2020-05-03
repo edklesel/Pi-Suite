d7144520171cb8b84355088f19d34f4c30ec7196
Edward Klesel <edwardklesel@gmail.com>
2020-05-03 10:34:47 +0100

    chmod in the wrong place

--- Monitoring/ProcMon/install.sh
+++ Monitoring/ProcMon/install.sh
@@ -6,9 +6,9 @@ echo "Installing ProcMon..."
 echo "- Downloading procmon from GitHub."
 wget -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/ProcMon/procmon.sh
 echo "- Moving procmon to /opt/monitoring/procmon"
-chmod u+x procmon
 mkdir -p /opt/monitoring/procmon
 mv procmon.sh /opt/monitoring/procmon
+chmod u+x /opt/monitoring/procmon
 sed -i "s/<procmonhost>/$pushgatehost/g" /opt/monitoring/procmon/procmon
 
 echo "- Downloading procmon service file"
