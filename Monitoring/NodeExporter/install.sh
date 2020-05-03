85642d170da9e1806f1d2095c7b1f83d30c24a29
Edward Klesel <edwardklesel@gmail.com>
2020-05-03 18:06:31 +0100

    Add node_exporter service

---
+++ Monitoring/NodeExporter/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo ""
+echo "Installing node_exporter"
+version="node_exporter-0.18.1.linux-armv7"
+
+echo "- Downloading from github"
+wget --no-check-certificate -q "https://github.com/prometheus/node_exporter/releases/download/v0.18.1/${version.tar}.gz"
+
+echo "- Extracting tarball"
+mkdir -p /opt/monitoring
+tar -xzf "${version}.tar.gz" -C /opt/monitoring
+mv "/opt/monitoring/$version" /opt/monitoring/node_exporter
+rm "${version}.tar.gz" -rf
+
+echo "- Creating node_exporter service"
+wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/NodeExporter/node_exporter.service
+mv node_exporter.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable node_exporter
+
+echo "- Service created. Start by running `service node_exporter start`"
