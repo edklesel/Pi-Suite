f75802a913d993e5ce03ac63eb685122845b238c
Edward Klesel <edwardklesel@gmail.com>
2020-05-03 10:33:38 +0100

    Add progress output and suppress command output

--- Monitoring/ProcMon/install.sh
+++ Monitoring/ProcMon/install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-echo "Installing ProcMon..."
 cd /tmp
 read -p "Pushgate Host IP Address: " pushgatehost
-wget https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/ProcMon/procmon.sh
-mv procmon.sh procmon
+echo ""
+echo "Installing ProcMon..."
+echo "- Downloading procmon from GitHub."
+wget -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/ProcMon/procmon.sh
+echo "- Moving procmon to /opt/monitoring/procmon"
 chmod u+x procmon
-mkdir /opt/monitoring
-mkdir /opt/monitoring/procmon
-mv procmon /opt/monitoring/procmon
+mkdir -p /opt/monitoring/procmon
+mv procmon.sh /opt/monitoring/procmon
 sed -i "s/<procmonhost>/$pushgatehost/g" /opt/monitoring/procmon/procmon
 
-wget https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/ProcMon/procmon.service
+echo "- Downloading procmon service file"
+wget -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/ProcMon/procmon.service
+echo "- Moving service file to systemd"
 mv procmon.service /etc/systemd/system/procmon.service
+echo "- Reloading systemctl daemon"
 systemctl daemon-reload
+echo "Fin"
