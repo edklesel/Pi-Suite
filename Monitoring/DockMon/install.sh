c4306a95cbdbc3884574f74c670ea68c5c0f253e
Edward Klesel <edwardklesel@gmail.com>
2020-05-08 16:05:47 +0100

    Create install files and docker files

---
+++ Monitoring/DockMon/install.sh
@@ -0,0 +1,17 @@
+echo "Installing dockmon"
+
+mkdir -p /opt/docker/dockmon
+cd /opt/docker/dockmon
+
+echo "- Downloading from Github"
+wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/dockmon.yml
+wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/Dockerfile_dockmon.yml
+wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/docker-compose.yml
+
+echo "- Creating dockmon service"
+wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/dockmon.service
+mv /opt/docker/dockmon/dockmon.service /etc/systemd/system/dockmon.service
+systemctl daemon-reload
+systemctl enable node_exporter
+
+echo "- Service created. Start by running 'service dockmon start'"
