264fe059e3a2648e017641a118d66f2df4449339
Edward Klesel <edwardklesel@gmail.com>
2020-05-09 11:54:52 +0100

    Remove service from install script and add docker-compose file

--- Monitoring/DockMon/install.sh
+++ Monitoring/DockMon/install.sh
@@ -5,12 +5,4 @@ cd /opt/docker/dockmon
 
 echo "- Downloading from Github"
 wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/dockmon.yml
-wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/Dockerfile
-
-echo "- Creating dockmon service"
-wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/dockmon.service
-mv /opt/docker/dockmon/dockmon.service /etc/systemd/system/dockmon.service
-systemctl daemon-reload
-systemctl enable node_exporter
-
-echo "- Service created. Start by running 'service dockmon start'"
+wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/docker-compose.yml
