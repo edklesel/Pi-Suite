500687470cfcc5e4b9b8bd83875251b049d57fc2
Edward Klesel <edwardklesel@gmail.com>
2020-05-09 12:20:22 +0100

    Change dockmon.cron so it isn't in the container

--- Monitoring/DockMon/install.sh
+++ Monitoring/DockMon/install.sh
@@ -5,4 +5,5 @@ cd /opt/docker/dockmon
 
 echo "- Downloading from Github"
 wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/dockmon.yml
+wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/dockmon.cron
 wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/docker-compose.yml
