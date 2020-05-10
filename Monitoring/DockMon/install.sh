a72223616f9d234192f8e56aff13268db94a240b
Edward Klesel <edwardklesel@gmail.com>
2020-05-10 14:06:02 +0100

    Remove install script

--- Monitoring/DockMon/install.sh
+++
@@ -1,9 +0,0 @@
-echo "Installing dockmon"
-
-mkdir -p /opt/docker/dockmon
-cd /opt/docker/dockmon
-
-echo "- Downloading from Github"
-wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/dockmon.yml
-wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/dockmon.cron
-wget --no-check-certificate -q https://raw.githubusercontent.com/edklesel/Pi-Suite/master/Monitoring/DockMon/docker-compose.yml
